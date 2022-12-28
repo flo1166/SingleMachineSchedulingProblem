e57ed95c49908a20b063e551dab7c6772c88b867
flo1166 <floriankorn93@web.de>
2022-12-28 10:56:53 +0100

    Change of data

--- src/solver/Solver.java
+++ src/solver/Solver.java
@@ -17,10 +17,10 @@ public class Solver {
 	public static void main(String[] args) {
 		
 		Job[] jobs = new Job[4];
-		jobs[0] = new Job("A", 1, 8, 10);
-		jobs[1] = new Job("B", 3, 0, 7);
-		jobs[2] = new Job("C", 5, 6, 11);
-		jobs[3] = new Job("D", 3, 1, 4);
+		jobs[0] = new Job("A", 6, 0, 14);
+		jobs[1] = new Job("B", 3, 1, 17);
+		jobs[2] = new Job("C", 8, 5, 15);
+		jobs[3] = new Job("D", 5, 8, 13);
 	}
 
 }
